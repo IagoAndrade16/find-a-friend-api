0621f9ca312b895b5d0badbdc1acbca043a16893
Iago Alexandre <iagoaap@outlook.com>
2023-06-21 19:53:46 -0300

    create pet controller added

--- src/http/routes/routes.ts
+++ src/http/routes/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { usersRoutes } from './users.routes'
+import { petsRoutes } from './pets.routes'
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(usersRoutes)
+  app.register(petsRoutes)
 }
