fd1b2803566fde579338367050eacc085bbc4769
Iago Alexandre <iagoaap@outlook.com>
2023-06-21 19:46:17 -0300

    create pet use case tests

--- src/usecases/factories/MakeCreateUserUseCase.ts
+++ src/usecases/factories/MakeCreateUserUseCase.ts
@@ -1,7 +1,7 @@
 import { UsersRepositoryImpl } from '@/repositories/prisma/UsersRepositoryImpl'
 import { CreateUserUseCase } from '../CreateUserUseCase'
 
-export function makeRegisterUseCase() {
+export function makeCreateUserUseCase() {
   const usersRepository = new UsersRepositoryImpl()
   const registerUseCase = new CreateUserUseCase(usersRepository)
 
